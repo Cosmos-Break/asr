32495ee4bbce60ef6f349119496cc0bf4e19dd08
Cosmos-Break <438854233@qq.com>
2022-08-25 10:22:31 +0800

    push

---
+++ main2.py
@@ -0,0 +1,60 @@
+from huggingsound import TrainingArguments, ModelArguments, SpeechRecognitionModel, TokenSet
+import os, random
+model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-chinese-zh-cn")
+output_dir = "output"
+
+# first of all, you need to define your model's token set
+# however, the token set is only needed for non-finetuned models
+# if you pass a new token set for an already finetuned model, it'll be ignored during training
+# tokens = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "'"]
+# token_set = TokenSet(tokens)
+
+# define your train/eval data
+train_data = []
+
+def read_data(dataset_name):
+    data_len = len(os.listdir(f'{dataset_name}/Split_TXT'))
+    print(data_len)
+    for cnt in range(1, data_len+1):
+        transcription = open(f'{dataset_name}/Split_TXT/{cnt}.txt', encoding='utf-8').readline().strip()
+        path = f'{dataset_name}/Split_WAV/{cnt}.wav'
+        train_data.append(
+            {"path": path, "transcription":transcription}
+        )
+read_data('Shanghai_Dialect_Conversational_Speech_Corpus')
+read_data('Shanghai_Dialect_Scripted_Speech_Corpus_Daily_Use_Sentence')
+random.shuffle(train_data)
+eval_ratio = 0.2
+index = int(len(train_data) * eval_ratio)
+eval_data = train_data[:index]
+train_data = train_data[index:]
+print('eval_data_len:', len(eval_data))
+print('train_data_len:', len(train_data))
+
+# fp16=True,
+
+training_args = TrainingArguments(
+    group_by_length=True,
+    num_train_epochs=3,
+    learning_rate=3e-4,
+    max_steps=1000,
+    eval_steps=200,
+    gradient_checkpointing=True,
+    weight_decay=0.005,
+    save_total_limit=2,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
+    gradient_accumulation_steps=2,
+)
+model_args = ModelArguments(
+    activation_dropout=0.1,
+    hidden_dropout=0.1,
+) 
+
+model.finetune(
+    output_dir, 
+    train_data=train_data, 
+    eval_data=eval_data, # the eval_data is optional
+    training_args=training_args,
+    model_args=model_args,
+)
